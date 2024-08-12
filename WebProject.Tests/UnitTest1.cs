6fbe3e0f04e322d4aca45f8926e1ec52da8ae12a
Ron Bernshtein <ronbernshtein277@gmail.com>
2024-08-12 13:15:49 +0300

    Implementing Tests with Moq

--- WebProject.Tests/UnitTest1.cs
+++ WebProject.Tests/UnitTest1.cs
@@ -1,3 +1,10 @@
+using Moq;
+using WebProject.Controllers;
+using WebProject.Services;
+using ZooLib.Models;
+using ZooLib.Repository;
+using Microsoft.Extensions.Logging;
+
 namespace WebProject.Tests
 {
     [TestClass]
@@ -5,8 +12,26 @@ namespace WebProject.Tests
     {
         // Moq FrameWork -- Optional
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetAnimal()
         {
+            var animal = new Animal
+            {
+                AnimalId = 1,
+                Name = "Dragon",
+                CategoryId = 1,
+                Age = 100,
+                Description = "scary"
+            };
+
+            var animalsRepository = new Mock<IRepository>();
+
+            animalsRepository.Setup(x => x.GetAnimal(It.IsAny<int>())).ReturnsAsync(animal);
+
+            var getAnimalById = animalsRepository.Object.GetAnimal(1);
+            Assert.IsNotNull(getAnimalById);
         }
     }
 }
+//var moqLogger = new Mock<ILogger<CatalogController>>();
+//var moqComment = new Mock<ICommentService>();
+//var catalogController = new CatalogController(animalsRepository.Object, moqLogger.Object, moqComment.Object);
